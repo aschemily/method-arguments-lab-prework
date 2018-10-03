30b3fd933560bf56dc03cb541ded5f9736840a73
Emily Asch <e.asch@me.com>
2018-10-03 15:57:13 +0000

    automatically backed up by learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -5,5 +5,5 @@ end
 
 
 def introduction_with_language(name,language)
-  "Hi, my name is #{name} and I am learning to program in #{language}\n."
+  "Hi, my name is #{name} and I am learning to program in #{language} \n."
 end
